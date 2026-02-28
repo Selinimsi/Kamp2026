1f39fdd6fefd95d068790e44bd95af12ab086bf5
selin yıldırım <sy248359@gmail.com>
2026-02-28 21:37:10 +0300

    kampa devam

---
+++ SQLQuery2.sql
@@ -0,0 +1,10 @@
+﻿Select CustomerID Id,CompanyName ŞirketAdı,Phone TelNo from Customers--Select komutu seçmemizi sağlar
+Select * from Customers where City='London'
+Select * from Customers where City='London' or City='Berlin'
+Select * from Products where CategoryID=1 and UnitPrice>=20
+
+Select * from Products order by ProductName 
+Select * from Products order by UnitPrice desc
+select CategoryID,COUNT(*) from Products group by CategoryID
+select CategoryID,COUNT(*) from Products group by CategoryID having COUNT(*)<10
+select CategoryID,COUNT(*) from Products where UnitPrice>20 group by CategoryID having COUNT(*)<10
